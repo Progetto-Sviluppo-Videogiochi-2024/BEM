fc43047f9793e693a472600a47db5738a495e3ca
ccorvino3 <c.corvino3@studenti.uniba.it>
2024-06-16 17:22:11 +0200

    Aggiunta: scena del menu, soldatino e c# per giocatore, collisioni e camera

---
+++ Assets/Scripts/Character/Soldier/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator playerAnim;
+    public Rigidbody playerRigid;
+    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+    public bool walking;
+    public Transform playerTrans;
+
+    void FixedUpdate()
+    {
+        /* Per il movimento del player */
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigid.velocity = transform.forward * w_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigid.velocity = -transform.forward * wb_speed * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+        /* Per le animazioni del player */
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerAnim.SetTrigger("walk");
+            playerAnim.ResetTrigger("idle");
+            walking = true;
+            //steps1.SetActive(true); suono dei passi
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnim.ResetTrigger("walk");
+            playerAnim.SetTrigger("idle");
+            walking = false;
+            //steps1.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerAnim.SetTrigger("run_backward");
+            playerAnim.ResetTrigger("idle");
+            //steps1.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.ResetTrigger("run_backward");
+            playerAnim.SetTrigger("idle");
+            //steps1.SetActive(false);
+        }
+
+        /* Per la rotazione del player */
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
+        }
+
+        /* Per lo sprint (corsa) del player */
+        if (walking == true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                //steps1.SetActive(false);
+                //steps2.SetActive(true);
+                w_speed = w_speed + rn_speed;
+                playerAnim.SetTrigger("run_forward");
+                playerAnim.ResetTrigger("walk");
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                //steps1.SetActive(true);
+                //steps2.SetActive(false);
+                w_speed = olw_speed;
+                playerAnim.ResetTrigger("run_forward");
+                playerAnim.SetTrigger("walk");
+            }
+        }
+    }
+}
