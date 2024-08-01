8863a7c1160e171b20a3a99cde93a9bbcdd25f01
ccorvino3 <c.corvino3@studenti.uniba.it>
2024-08-01 13:55:15 +0200

    Modifiche in MainMenu
    Modificato la scena MainMenu:
    - Creato ConfirmExitUI: canvas con un panel dove il giocatore deve confermare o negare di uscire dal gioco
    - Cominciato a settare la logica nel MainMenu.cs: feature del canvas accessibili direttamente premendo tasti della Keyboard
    - Tolto ExitGame.cs in quanto la stessa responsabilità la ha una funzione di MainMenu.cs
    - Provato a creare AudioManager.cs che gestiva una responsabilità di MainMenu.cs sull'audio ma dava errore sulle "direttive Unity or riferimenti Assembly"

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,73 +1,80 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro; // Importa il namespace di TextMeshPro
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private string sceneName;
-    [SerializeField] private AudioClip backgroundMusic;
+    [Header("Game Settings")]
+    [SerializeField] private string sceneName; // Nome della scena da caricare
+
+    [Header("UI Glitch Settings")]
     [SerializeField] private Color glitchColor = Color.red; // Colore del glitch
     [SerializeField] private float glitchDuration = 0.1f; // Durata di ogni glitch
     [SerializeField] private float glitchInterval = 0.5f; // Intervallo tra i glitch
     [SerializeField] private float shakeIntensity = 5f; // Intensità del tremolio
 
-    private AudioSource audioSource;
+    [Header("UI Elements")]
+    [SerializeField] GameObject nameGameObject; // Riferimento all'oggetto NameGame
     private TextMeshProUGUI nameGameText; // Campo per il componente TextMeshProUGUI
-    private Color originalColor;
-    private Vector3 originalPosition;
-    private Quaternion originalRotation;
+    private Color originalColor; // Colore originale del testo
+    private Vector3 originalPosition; // Posizione originale del testo
+    private Quaternion originalRotation; // Rotazione originale del testo
+
+    [Header("Audio Settings")]
+    [SerializeField] private AudioClip backgroundMusic; // Clip audio per la musica di sottofondo
+    private AudioSource audioSource; // AudioSource per la musica di sottofondo
 
     void Start()
     {
-        // Aggiungi un componente AudioSource se non esiste
-        audioSource = gameObject.AddComponent<AudioSource>();
+        // Avvia la musica di sottofondo
+        PlayAudio();
 
-        // Imposta il clip audio
-        audioSource.clip = backgroundMusic;
+        // Crea l'effetto glitch sul testo
+        CreateGlitchText();
+    }
 
-        // Configura l'AudioSource
-        audioSource.loop = true; // Imposta loop su true per riprodurre l'audio in loop
-        audioSource.playOnAwake = true;
+    void Update()
+    {
+        ActivateMenuFromKeyboard();
+    }
 
-        // Riproduci l'audio
-        audioSource.Play();
-
-        // Trova l'oggetto NameGame e il suo TextMeshProUGUI
-        GameObject nameGameObject = GameObject.Find("MenuCanvas/StartUI/NameGame");
-        if (nameGameObject != null)
-        {
-            nameGameText = nameGameObject.GetComponent<TextMeshProUGUI>();
-            if (nameGameText != null)
-            {
-                originalColor = nameGameText.color;
-                originalPosition = nameGameText.transform.position;
-                originalRotation = nameGameText.transform.rotation;
-                StartCoroutine(GlitchEffectCoroutine());
-            }
-            else
-            {
-                Debug.LogError("Componente TextMeshProUGUI non trovato su NameGame.");
-            }
-        }
-        else
-        {
-            Debug.LogError("Oggetto NameGame non trovato nella gerarchia.");
-        }
+    private void ActivateMenuFromKeyboard()
+    {
+        // TODO: implementare le feature del menu con la tastiera
+        // problema: troppi riferimenti se come attributi del cs
+        // oppure usare metodi che prendono come stringa i path dei GameObject
     }
 
     public void PlayGame()
     {
         // Ferma la musica quando si avvia il gioco
-        audioSource.Stop();
+        StopAudio();
         SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
     {
         // Ferma la musica e chiudi il gioco
-        audioSource.Stop();
+        StopAudio();
         Application.Quit();
+    }
+
+    private void CreateGlitchText()
+    {
+        // GameObject nameGameObject = GameObject.Find("Menu/StartUI/NameGame"); // Trova l'oggetto NameGame e il suo TextMeshProUGUI
+        if (nameGameObject != null)
+        {
+            if (nameGameObject.TryGetComponent<TextMeshProUGUI>(out nameGameText))
+            {
+                originalColor = nameGameText.color;
+                originalPosition = nameGameText.transform.position;
+                originalRotation = nameGameText.transform.rotation;
+                StartCoroutine(GlitchEffectCoroutine());
+            }
+            else Debug.LogError("Componente TextMeshProUGUI non trovato su NameGame.");
+        }
+        else Debug.LogError("Oggetto NameGame non trovato nella gerarchia.");
     }
 
     private IEnumerator GlitchEffectCoroutine()
@@ -83,30 +90,47 @@ public class MainMenu : MonoBehaviour
                 float shakeTime = glitchDuration;
                 while (shakeTime > 0)
                 {
-                    nameGameText.transform.position = originalPosition + Random.insideUnitSphere * shakeIntensity;
-                    nameGameText.transform.rotation = Quaternion.Euler(
+                    nameGameText.transform.SetPositionAndRotation(originalPosition + Random.insideUnitSphere * shakeIntensity, Quaternion.Euler(
                         originalRotation.eulerAngles + new Vector3(
                             Random.Range(-shakeIntensity, shakeIntensity),
                             Random.Range(-shakeIntensity, shakeIntensity),
                             Random.Range(-shakeIntensity, shakeIntensity)
                         )
-                    );
+                    ));
                     shakeTime -= Time.deltaTime;
                     yield return null;
                 }
 
                 // Ripristina la posizione e la rotazione originali
-                nameGameText.transform.position = originalPosition;
-                nameGameText.transform.rotation = originalRotation;
+                nameGameText.transform.SetPositionAndRotation(originalPosition, originalRotation);
 
                 // Ripristina il colore
                 nameGameText.color = originalColor;
                 yield return new WaitForSeconds(glitchInterval);
             }
-            else
-            {
-                yield break; // Interrompe il ciclo se l'elemento TextMeshProUGUI non è stato assegnato
-            }
+            else yield break; // Interrompe il ciclo se l'elemento TextMeshProUGUI non è stato assegnato
         }
+    }
+
+    private void PlayAudio()
+    {
+        // Aggiungi un componente AudioSource se non esiste
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        // Imposta il clip audio
+        audioSource.clip = backgroundMusic;
+
+        // Configura l'AudioSource
+        audioSource.loop = true; // Imposta loop su true per riprodurre l'audio in loop
+        audioSource.playOnAwake = true;
+
+        // Riproduci l'audio
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        // Interrompi l'audio
+        audioSource.Stop();
     }
 }
