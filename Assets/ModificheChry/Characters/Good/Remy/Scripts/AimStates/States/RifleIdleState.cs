e0e2c33bc848a17f8b62ee3c5216377177698f3e
ccorvino3 <c.corvino3@studenti.uniba.it>
2024-11-08 00:53:00 +0100

    Rifatto Stefano
    Cancellato gli altri stefano fasulli e falsi ahaha
    Aggiunto di nuovo tutte le animazioni per evitare quell'errore
    Risolto da solo il problema del crouch idle, ma cambiargli i valori del transform della camera
    Per alcune animazione vanno settati gli offset (per farla sembrare più reale)

---
+++ Assets/ModificheChry/Characters/Good/Remy/Scripts/AimStates/States/RifleIdleState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RifleIdleState : AimBaseState
+{
+    public override void EnterState(AimStateManager aim)
+    {
+        aim.animator.SetBool("aiming", false);
+        aim.currentFov = aim.idleFov;
+    }
+
+    public override void UpdateState(AimStateManager aim)
+    {
+        if (Input.GetKey(KeyCode.Mouse1)) aim.SwitchState(aim.rifleIdleAimState); // Destro del mouse
+    }
+}
---
+++ Assets/ModificheChry/Characters/Good/Remy/Scripts/MovementStates/States/CrouchState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CrouchState : MovementBaseState
+{
+    public override void EnterState(MovementStateManager movement) => movement.animator.SetBool("crouching", true);
+    
+    public override void UpdateState(MovementStateManager movement)
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) ExitState(movement, movement.runningState);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (movement.moveDirection.magnitude < 0.1f) ExitState(movement, movement.idleState);
+            else ExitState(movement, movement.walkingState);
+        }
+
+        movement.currentMoveSpeed = movement.v < 0 ? movement.crouchBackSpeed : movement.crouchSpeed;
+    }
+
+    void ExitState(MovementStateManager movement, MovementBaseState newState)
+    {
+        movement.animator.SetBool("crouching", false);
+        movement.SwitchState(newState);
+    }
+}
