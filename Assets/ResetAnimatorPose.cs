79eef3d9d948d5e26787e00b70dc5c683f8c747f
ccorvino3 <c.corvino3@studenti.uniba.it>
2024-11-29 01:11:03 +0100

    Script per resettare i rig di stefano
    Resetta i rig dell'animator di Stefano
    Settato il Transform dello zaino in scena2

---
+++ Assets/ResetAnimatorPose.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ResetAnimatorPose : MonoBehaviour
+{
+    private void OnValidate() // Richiamato ogni volta che modifichi qualcosa in Edit Mode
+    {
+        Animator animator = GetComponent<Animator>();
+        if (animator)
+        {
+            animator.Rebind(); // Resetta lo stato del rig
+            animator.Update(0); // Aggiorna l'animatore alla sua posa iniziale
+        }
+    }
+}
