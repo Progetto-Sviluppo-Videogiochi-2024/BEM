ed7311c54abbaca8784aa89333e90238d870d48b
ccorvino3 <c.corvino3@studenti.uniba.it>
2024-12-27 03:52:09 +0100

    AI e animazioni per Gaia
    Configurata Gaia con animator (idle, run, walk), Agent (AI) e collider
    - sta giusto un pochino sospesa in aria
    - lagga ogni tanto per fermarsi (devo ancora fixarlo)
    Logica per le AI (non per tutte credo)
    
    DIfetti notati sulla scena:
    - mi si è buggato il personaggio e per i collider non riuscivo più a passare...lì dove ci sono i collider e gli alberi, spostate gli alberi e distanziateli un po' dagli asset di scena
    - poi seconda cosa, rifate i percorsi perché alcuni sono sopraelevati e non riesce a salire, ma non perché sta configurato male ma perché la collinetta ha una pendenza della madonna
    - ah non generate mai più asset formati da più asset (zona falò militare), ci ho messo tempo a fare i collider...
    - ho trovato un oggetto vicino al falò militare che stava sotto terra quindi controllare se ce ne sono anche altri

---
+++ Assets/ModificheChry/Task/Intro scena3/ZonaCamping.cs
@@ -0,0 +1,87 @@
+using System;
+using DialogueEditor;
+using UnityEngine;
+
+public class ZonaCamping : MonoBehaviour
+{
+    [Header("Settings")]
+    #region Settings
+    private bool isPreHitBallFinished = false; // Flag per verificare se è finito il dialogo Pre-HitBall di Angelica e Jacob
+    private bool isPostHitBallJA = false; // Flag per verificare se è finito il dialogo Post-HitBall di Angelica e Jacob
+    #endregion
+
+    [Header("References")]
+    #region References
+    public NPCConversation[] conversations; // Array di dialoghi per la zona camping
+    private ConversationManager conversationManager; // Riferimento al ConversationManager
+    private BooleanAccessor booleanAccessor; // Riferimento al BooleanAccessor
+    private Transform gaiaSitting; // Riferimento al transform di Gaia seduta
+    private Transform stefanoSitting; // Riferimento al transform di Stefano seduto
+    private Transform jacob; // Riferimento a Jacob (AI)
+    private Transform angelica; // Riferimento ad Angelica (AI)
+    public Transform player; // Riferimento al giocatore (Player)
+    public Transform gaia; // Riferimento a Gaia (AI)
+    #endregion
+
+    void Start()
+    {
+        var characters = transform.GetChild(0);
+        gaiaSitting = characters.GetChild(0);
+        stefanoSitting = characters.GetChild(1);
+        // jacob = characters.GetChild(2);
+        // angelica = characters.GetChild(3);
+
+        booleanAccessor = BooleanAccessor.istance;
+        conversationManager = ConversationManager.Instance;
+        conversationManager.StartConversation(conversations[0]); // Pre-HitBall di Angelica e Jacob
+    }
+
+    void Update() // Invocarle come event del DialogueEditor nei relativi nodi e dialoghi -> fare delle sotto-funzioni per evitare l'uso di Update => efficienza++
+    {
+        if (booleanAccessor.GetBoolFromThis("videoMutant")) { enabled = false; return; }
+
+        // Se è finito il primo dialogo e Angelica ha calciato la palla allora Post-HitBall di Angelica e Jacob
+        if (!isPreHitBallFinished && booleanAccessor.GetBoolFromThis("preHitBallJA") && HasBallBeenKicked())
+        {
+            isPreHitBallFinished = true; // Si ripete una sola volta almeno
+            conversationManager.StartConversation(conversations[1]); // Post-HitBall di Angelica e Jacob
+        }
+
+        // Se Post-HitBall è TRUE allora Angelica e Jacob si allontanano e scompaiono
+        if (!isPostHitBallJA && booleanAccessor.GetBoolFromThis("postHitBallJA"))
+        {
+            // Fare una classe astratta per le due AI così seguono una logica comune per poi allontanarsi e scomparire (usare un flag dello script)
+            if (Input.GetKeyDown(KeyCode.Z)/*angelica.isArrived && jacob.isArrived*/) // per testing
+            {
+                // jacob.gameObject.SetActive(false);
+                // angelica.gameObject.SetActive(false);
+                isPostHitBallJA = true; // Si ripete una sola volta almeno
+                conversationManager.StartConversation(conversations[2]); // Post-HitBall di Stefano e Gaia
+            }
+        }
+
+        if (booleanAccessor.GetBoolFromThis("postHitBallSG"))
+        {
+            // Dopo che termina il dialogo di Stefano e Gaia allora parte il video del mutante che attacca i militari @marcoWarrior @ccorvino3
+            booleanAccessor.SetBoolOnDialogueE("videoMutant");
+        }
+
+        if (booleanAccessor.GetBoolFromThis("videoMutant"))
+        {
+            // Dopo il video del mutante allora Stefano e Gaia si alzano e camminano
+            gaiaSitting.gameObject.SetActive(false);
+            stefanoSitting.gameObject.SetActive(false);
+            gaia.gameObject.SetActive(true);
+            player.gameObject.SetActive(true);
+            // AI di Gaia che cammina seguendo Stefano (Player)
+        }
+    }
+
+    private bool HasBallBeenKicked()
+    {
+        // Per testing, Nico, questa parte è da cancellare
+        if (Input.GetKeyDown(KeyCode.A)) return true;
+        else return false;
+        throw new NotImplementedException("Nicola implementa qui la funzione per verificare se la palla è stata colpita da Angelica.");
+    }
+}
