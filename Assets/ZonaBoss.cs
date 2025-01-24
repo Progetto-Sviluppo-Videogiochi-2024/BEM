b549dced2590353c03e9ba9d26373b8285cb1665
ccorvino3 <c.corvino3@studenti.uniba.it>
2025-01-24 19:00:47 +0100

    Inizio zona boss
    Spostato oggetti in GO miniera
    Script per zona boss

---
+++ Assets/ZonaBoss.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ZonaBoss : MonoBehaviour
+{
+    public Transform gaia; // 
+    public Transform angelica;
+    public Transform jacob;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
